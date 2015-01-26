3276f7710b477098145e2d33a25a8e3a3c3cf991
gavic007 <mylesgavic@gmail.com>
2015-01-26 15:37:10 -0600

    Tests for both GPA methods pass successfully.

--- public/javascript/clientJavascript.spec.js
+++ public/javascript/clientJavascript.spec.js
@@ -21,6 +21,29 @@ describe('testing basic functions', function(){
     it('this test should add 2 to a number', function(){
         expect(addTwo(7)).toBe(9);
     });
-
+    //this test should calculate the correct GPA.
+    it('this test should calculate the correct GPA', function(){
+        expect(calculateGPA('A', 4)).toBe(4);
+    });
+    //this test should calculate the correct GPA.
+    it('this test should calculate the correct GPA', function(){
+        expect(calculateGPA('C', 2)).toBe(2);
+    });
+    //this test should calculate the correct GPA.
+    it('this test should calculate the correct GPA', function(){
+        expect(calculateGPA('F', 4)).toBe(0);
+    });
+    //this test should calculate the correct GPA.
+    it('this test should calculate the correct GPA', function(){
+        expect(totalGPA(4, 4, 4)).toBe(4);
+    });
+    //this test should calculate the correct GPA.
+    it('this test should calculate the correct GPA', function(){
+        expect(totalGPA(4, 2, 3)).toBe(3);
+    });
+    //this test should calculate the correct GPA.
+    it('this test should calculate the correct GPA', function(){
+        expect(totalGPA(0, 4, 2)).toBe(2);
+    });
 });
 
